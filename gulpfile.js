6fc41ac83ca9565200d81a1cd1065af33eb3c3d4
grighes <glauber.righes@gmail.com>
2018-02-22 14:18:43 -0300

    carrousel

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var minifyJS = require('gulp-minify');
 
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCSS())
     .pipe(gulp.dest('./public/stylesheets'));
+});
+
+gulp.task('js', function(){
+  return gulp.src('./src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(minifyJS())
+    .pipe(gulp.dest('./public/javascripts'))
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('js:watch', function () {
+  gulp.watch('./src/js/**/*.js', ['js']);
+});
+
+gulp.task('default', ['sass', 'js']);
